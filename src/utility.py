96b7ba5338cdcd1660d4603780cabea9becef6a5
yh <coonhgf@gmail.com>
2021-08-05 17:59:54 +0800

    210805_big_mdf_1759

--- src/utility.py
+++ src/utility.py
@@ -224,6 +224,27 @@ class checkpoint():
                         # print("shape of np_rst_oristyle_i16={0}".format(np_rst_oristyle_i16.shape))
                         # dcm_data.Rows, dcm_data.Columns = np_rst_oristyle_i16.shape
                         ### =>
+                        # np_rst_round = np.round(np_rst, 0)
+                        # np_rst_clip = np.clip(np_rst_round, -2048.0, 3071.0)
+                        # ####np_rst_clip = np.clip(np_rst_round, -1024.0, 3071.0)
+                        # # convert back to ori-style
+                        # the_intercept = dcm_data.RescaleIntercept
+                        # the_slope = dcm_data.RescaleSlope
+                        # if the_slope == 0:
+                        #     print("\n\n\n Error, the_slope=0 in file:{0}".format(ori_dcm_fp))
+                        # np_rst_oristyle = (np_rst_clip - the_intercept) / the_slope
+                        # np_rst_oristyle_i16 = np_rst_oristyle.astype(np.int16)
+                        # ####dcm_data.PixelData = np_rst_oristyle_i16.tostring()
+                        # dcm_data.PixelData = np_rst_oristyle_i16.tobytes()
+                        # print("shape of np_rst_oristyle_i16={0}".format(np_rst_oristyle_i16.shape))
+                        # dcm_data.Rows, dcm_data.Columns = np_rst_oristyle_i16.shape
+                        # dcm_data.BitsStored = 16
+                        # dcm_data.HighBit = 15
+                        # #dcm_data.SmallestImagePixelValue = 0
+                        # #dcm_data.LargestImagePixelValue = 4095
+                        # # save
+                        # dcm_data.save_as(filename)
+                        ### =>
                         np_rst_round = np.round(np_rst, 0)
                         np_rst_clip = np.clip(np_rst_round, -2048.0, 3071.0)
                         ####np_rst_clip = np.clip(np_rst_round, -1024.0, 3071.0)
@@ -233,13 +254,14 @@ class checkpoint():
                         if the_slope == 0:
                             print("\n\n\n Error, the_slope=0 in file:{0}".format(ori_dcm_fp))
                         np_rst_oristyle = (np_rst_clip - the_intercept) / the_slope
-                        np_rst_oristyle_i16 = np_rst_oristyle.astype(np.uint16)
+                        np_rst_oristyle_i16 = np_rst_oristyle.astype(np.int16)
                         ####dcm_data.PixelData = np_rst_oristyle_i16.tostring()
                         dcm_data.PixelData = np_rst_oristyle_i16.tobytes()
                         print("shape of np_rst_oristyle_i16={0}".format(np_rst_oristyle_i16.shape))
                         dcm_data.Rows, dcm_data.Columns = np_rst_oristyle_i16.shape
                         dcm_data.BitsStored = 16
                         dcm_data.HighBit = 15
+                        dcm_data.PixelRepresentation = 0
                         #dcm_data.SmallestImagePixelValue = 0
                         #dcm_data.LargestImagePixelValue = 4095
                         
